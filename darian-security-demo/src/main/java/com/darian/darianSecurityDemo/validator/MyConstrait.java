801a48419f1f1fa4eb5e84a1c4dd76655922c9c7
Darian <1934849492@qq.com>
2019-01-01 18:14:55 +0800

    自定义校验注解

--- darian-security-demo/src/main/java/com/darian/darianSecurityDemo/validator/MyConstrait.java
+++ darian-security-demo/src/main/java/com/darian/darianSecurityDemo/validator/MyConstrait.java
@@ -1,23 +1,23 @@
-//package com.darian.darianSecurityDemo.validator;
-//
-//import javax.validation.Constraint;
-//import javax.validation.Payload;
-//import java.lang.annotation.ElementType;
-//import java.lang.annotation.Retention;
-//import java.lang.annotation.RetentionPolicy;
-//import java.lang.annotation.Target;
-//
-///***
-// *
-// */
-//@Target({ElementType.METHOD,ElementType.FIELD})
-//@Retention(RetentionPolicy.RUNTIME)
-//@Constraint(validatedBy = {MyContstrainValidator.class})
-//public @interface MyConstrait {
-//
-//    String message() default "MyConstrait default message";
-//
-//    Class<?>[] groups() default { };
-//
-//    Class<? extends Payload>[] payload() default { };
-//}
+package com.darian.darianSecurityDemo.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/***
+ *
+ */
+@Target({ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {MyContstrainValidator.class})  // 这个注解用的是某一个类的校验逻辑
+public @interface MyConstrait {
+
+    String message() default "MyConstrait default message";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}

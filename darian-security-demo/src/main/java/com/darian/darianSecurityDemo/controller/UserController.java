d8466522b50d12f5822792bf8dec1a1e69383c81
Darian <1934849492@qq.com>
2019-01-01 18:15:23 +0800

    文件，用户的处理

--- darian-security-demo/src/main/java/com/darian/darianSecurityDemo/controller/UserController.java
+++ darian-security-demo/src/main/java/com/darian/darianSecurityDemo/controller/UserController.java
@@ -1,46 +1,82 @@
 package com.darian.darianSecurityDemo.controller;
 
 import com.darian.darianSecurityDemo.dto.User;
+import com.darian.darianSecurityDemo.exception.Response;
 import com.darian.darianSecurityDemo.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
+import static com.darian.darianSecurityDemo.exception.Response.*;
+
+@Slf4j
 @Api(description = "用户服务接口")
 @RestController
 @AllArgsConstructor
 public class UserController {
     private final UserService userServiceImpl;
 
-    @ApiOperation("得到所有的 User 的集合")
-    @GetMapping("/users")
-    public List<User> users() {
-        return userServiceImpl.users();
+    @PostMapping("/user")
+    public User userCreate(@RequestBody @Validated User user/*, BindingResult errors*/){
+/*        if(errors.hasErrors()){
+            errors.getAllErrors().stream().forEach(objectError -> {
+                System.out.println(objectError.getDefaultMessage());
+            });
+        }*/
+
+        user.setId(1L);
+        return user;
     }
 
+    @ApiOperation("得到所有的 User 的集合")
+    @GetMapping("/users")
+    public Response<List<User>> users() {
+        return success(userServiceImpl.users());
+    }
+
+//    @JsonView(User.UserSimpleView.class)
     @ApiOperation("根据 id 获取对应的用户信息")
     @GetMapping("/user")
     @ApiImplicitParam(name = "id", dataType = "Long", value = "id")
-    public User user(@RequestParam(required = false) Long id) {
-        return userServiceImpl.find(id);
+    public List<User> user(@RequestParam(value = "id", required = false) Long id,
+                           @PageableDefault(page = 2,
+                                   size = 10,
+                                   sort = "user, asc") Pageable pageable) {
+
+
+        return Arrays.asList(new User[]{
+                new User(12L, "darian", "sss", new Date()),
+                new User(),
+                new User()});
+//        return userServiceImpl.find(id);
+    }
+
+//    @JsonView(User.UserDetailView.class)
+    @GetMapping("/user/{id:\\d+}")
+    public User getInfo(@PathVariable Long id) {
+        return new User(id, "darian", "123456", new Date());
     }
 
     @ApiOperation(value = "添加一个用户",
             notes = "如果没有用户传进来，则后台随机生成一个用户信息")
-    @GetMapping("/user/add")
-    public boolean add(@RequestBody(required = false) User user) {
+    @PostMapping("/user/add")
+    public boolean add(@RequestBody User user) {
         return userServiceImpl.add(user);
     }
+
 
     @GetMapping("/meAuthentication")
     @ApiOperation("获取当前的用户的登陆信息Authentication")
--- darian-security-demo/src/main/java/com/darian/darianSecurityDemo/dto/User.java
+++ darian-security-demo/src/main/java/com/darian/darianSecurityDemo/dto/User.java
@@ -4,24 +4,36 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.hibernate.validator.constraints.NotBlank;
 
-import java.util.Collection;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User implements UserDetails {
+public class User {
+//    public interface UserSimpleView {
+//    }
+
+//    public interface UserDetailView extends UserSimpleView {
+//    }
+
+//    @JsonView(UserSimpleView.class)
     @ApiModelProperty(value = "id")
     private Long id;
+    @NotBlank
+//    @JsonView(UserSimpleView.class)
     @ApiModelProperty(value = "用户名")
     private String username;
+//    @JsonView(UserDetailView.class)
     @ApiModelProperty(value = "密码")
     private String password;
-    private Collection<? extends GrantedAuthority> authorities;
-    private boolean accountNonExpired;
-    private boolean accountNonLocked;
-    private boolean credentialsNonExpired;
-    private boolean enabled;
+
+    private Date birthday;
+
+//    private Collection<? extends GrantedAuthority> authorities;
+//    private boolean accountNonExpired;
+//    private boolean accountNonLocked;
+//    private boolean credentialsNonExpired;
+//    private boolean enabled;
 }

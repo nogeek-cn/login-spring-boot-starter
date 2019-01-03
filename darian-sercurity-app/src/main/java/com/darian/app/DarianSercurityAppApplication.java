55eb0e7b5e67efed16edb9c3d3a7935d5a807dc1
Darian <1934849492@qq.com>
2019-01-03 10:09:10 +0800

    app 项目修正

--- darian-sercurity-app/src/main/java/com/darian/app/DarianSercurityAppApplication.java
+++ darian-sercurity-app/src/main/java/com/darian/app/DarianSercurityAppApplication.java
@@ -1,4 +1,4 @@
-package com.darian.darianSercurityApp;
+package com.darian.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
